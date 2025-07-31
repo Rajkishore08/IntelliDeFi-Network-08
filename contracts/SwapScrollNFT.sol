52e371063297520627c58056cb45fbb48a608e8b
Rajkishore08 <rajkishorerk082004@gmail.com>
2025-07-31 21:22:13 +0530

    testt

--- contracts/SwapScrollNFT.sol
+++ contracts/SwapScrollNFT.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 /**
  * @title SwapScrollNFT
